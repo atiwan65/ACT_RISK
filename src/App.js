07cf4c321f534b9cd045deea9358a988e9c6ff32
atiwan65 <atiwan.suai@northbkk.ac.th>
2023-08-22 04:44:54 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import RegistrationForm from './RegistrationForm';
+import './FormStyles.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="h2">Registration Form</div>
+      <RegistrationForm />
+    </div>
+  );
+}
+
+export default App;
